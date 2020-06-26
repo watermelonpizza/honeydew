c190945b7193ccf0b8183c7c5ec2ffeb0ce8496e
Daniel Miller <watermelonpizza@melonstudios.net>
2020-06-26 21:55:01 +1000

    :chart_with_upwards_trend: add application insights (optional)

--- src/Honeydew/Startup.cs
+++ src/Honeydew/Startup.cs
@@ -46,6 +46,8 @@ namespace Honeydew
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddApplicationInsightsTelemetry();
+
             var databaseTypeString = Configuration["Database:Type"];
 
             if (!Enum.TryParse(databaseTypeString, out DatabaseType type))
